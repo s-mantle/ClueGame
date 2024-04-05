783c616ebba31afa0350cdd8cda3f0451e248b99
Albitr <isenhart.ben@gmail.com>
2024-04-05 12:15:28 -0600

    Starting

--- src/clueGame/GameControlPanel.java
+++ src/clueGame/GameControlPanel.java
@@ -1,19 +1,51 @@
 package clueGame;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import java.awt.BorderLayout;
 import java.awt.Color;
 
 public class GameControlPanel extends JPanel {
-
+	// Reference the layout template from C22A-1 to understand what each instance variable represents
+	JPanel mainPanel;
+	JPanel upperHalf, lowerHalf;
+	
+	JPanel upperLeftMost, upperLeftMiddle;
+	JButton upperRightMiddle, upperRightMost;
+	
+	JLabel turnLabel; JTextField turnPlayerName;
+	JLabel rollLabel; JTextField rollValue;
+	
+	JPanel lowerLeft, lowerRight;
+	JTextField guessText, guessResults;
+	
 	/**
 	 * Constructor for the panel, it does 90% of the work
 	 */
 	public GameControlPanel()  {
-//		setTitle("Game Control Panel");
-		setSize(300,300);
+		mainPanel = new JPanel();
+		mainPanel.setVisible(true);
 		
-//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		upperHalf = new JPanel(); lowerHalf = new JPanel();
+		add(upperHalf, BorderLayout.NORTH); add(lowerHalf, BorderLayout.SOUTH);
+		upperHalf.setVisible(true); lowerHalf.setVisible(true);
+		
+		upperLeftMost = new JPanel(); upperLeftMiddle = new JPanel();
+		upperRightMiddle = new JButton(); upperRightMost = new JButton();
+		// Use GridLayouts when needed
+		
+		
+		lowerLeft = new JPanel(); lowerRight = new JPanel();
+		add(lowerLeft, lowerHalf); add(lowerRight, lowerHalf);
+		lowerLeft.setVisible(true); lowerRight.setVisible(true);
+		
+		guessText = new JTextField("I have no guess!"); guessResults = new JTextField("So you have nothing?");
+		guessText.setVisible(true); guessResults.setVisible(true);
+		add(guessText, lowerLeft); add(guessResults, lowerRight);
 	}
 	
 	
@@ -24,9 +56,9 @@ public class GameControlPanel extends JPanel {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-//		GameControlPanel panel = new GameControlPanel();  // create the panel
+		GameControlPanel panel = new GameControlPanel();  // create the panel
 		JFrame frame = new JFrame();  // create the frame 
-//		frame.setContentPane(panel); // put the panel in the frame
+		frame.setContentPane(panel); // put the panel in the frame
 		frame.setSize(750, 180);  // size the frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
 		frame.setVisible(true); // make it visible
